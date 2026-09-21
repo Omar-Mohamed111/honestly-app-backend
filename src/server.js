e58f16fcca94fd8239e8a35284fb966eedcb7fca
Omar Hamed <om893177@gmail.com>
2026-09-21 18:57:17 +0300

    Fix MongoDB connection startup

--- src/server.js
+++ src/server.js
@@ -1,15 +1,21 @@
 require("dotenv").config();
+
 const app = require("./app");
 const PORT = 3000;
 
 const connectDB = require("./config/db");
-connectDB();
 
+const startServer = async () => {
+  try {
+    await connectDB();
 
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT} ✅`);
+    });
+  } catch (error) {
+    console.error("MongoDB Connection Failed ❌");
+    console.error(error.message);
+  }
+};
 
-
-
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT} ✅`);
-});
+startServer();
