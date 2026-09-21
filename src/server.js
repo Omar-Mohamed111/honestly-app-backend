361ad49f1f9be3880aebb4aeb9a1ef1fa466378b
Omar Hamed <om893177@gmail.com>
2026-09-21 20:01:30 +0300

    Prepare backend for Vercel

--- src/server.js
+++ src/server.js
@@ -1,21 +1,5 @@
 require("dotenv").config();
 
 const app = require("./app");
-const PORT = 3000;
 
-const connectDB = require("./config/db");
-
-const startServer = async () => {
-  try {
-    await connectDB();
-
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT} ✅`);
-    });
-  } catch (error) {
-    console.error("MongoDB Connection Failed ❌");
-    console.error(error.message);
-  }
-};
-
-startServer();
+module.exports = app;
